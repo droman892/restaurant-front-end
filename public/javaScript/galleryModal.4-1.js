730402b704b011848bd1932f861241ac897aff11
droman892 <droman892@gmail.com>
2021-07-22 18:57:03 -0400

    updated

--- public/javaScript/galleryModal.4-1.js
+++ public/javaScript/galleryModal.4-1.js
@@ -1,7 +1,5 @@
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg10");
 var modalImg = document.getElementById("imgX");
 var captionText = document.getElementById("caption");
@@ -11,10 +9,8 @@ img.onclick = function () {
     captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
